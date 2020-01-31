6f72200c043d0d0de09472aacb952f8df45cdce8
Magda Ambrozy <magda.ambrozy@gmail.com>
2020-01-31 13:19:06 +0100

    Add dependencies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import React, { Component } from 'react';
+import './App.css';
+import Palette from './Palette'
+import seedPalettes from './seedPalettes'
+
+class App extends Component {
+  // constructor(props) {
+  //   super(props)
+
+  //   this.state = {
+         
+  //   }
+  // }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
