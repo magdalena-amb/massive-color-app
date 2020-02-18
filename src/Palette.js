02c8827ea623869208301cf386c1981adc6faa8d
Magda Ambrozy <magda.ambrozy@gmail.com>
2020-02-18 10:43:15 +0100

    Add footer

--- src/Palette.js
+++ src/Palette.js
@@ -19,7 +19,7 @@ export default class Palette extends Component {
     }
 
     render() {
-        const { colors } = this.props.palette;
+        const { colors, paletteName, emoji } = this.props.palette;
         const { level, format } = this.state;
         const colorBoxes = colors[level].map(color => (
             < ColorBox background={color[format]} name={color.name} />
@@ -31,7 +31,10 @@ export default class Palette extends Component {
                  <div className='Palette-colors'>
                      { colorBoxes }
                  </div>
-                 {/* footer */}
+                 <footer className='Palette-footer'>
+                     {paletteName}
+                     <span className='emoji'>{emoji}</span>
+                 </footer>
             </div>
         )
     }
