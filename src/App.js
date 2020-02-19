cf219731869dfc379ebded404ac2e77334e464cc
Magda Ambrozy <magda.ambrozy@gmail.com>
2020-02-19 14:36:05 +0100

    Display colors in SingleColorPalette

--- src/App.js
+++ src/App.js
@@ -22,7 +22,12 @@ class App extends Component {
         <Route exact 
         path="/palette/:id" 
         render={(routeProps) => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))}/> }/>
-        <Route exact path='/palette/:paletteId/:colorId' render={() => <SingleColorPalette/>}  />
+        <Route exact
+         path='/palette/:paletteId/:colorId' 
+         render={(routeProps) => <SingleColorPalette 
+            palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))}
+            colorId={routeProps.match.params.colorId}
+            /> }  />
       </Switch>
     )
   }
